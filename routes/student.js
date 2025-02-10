d2eae21c47e9b738d9c41668cd7537a5096c3824
Prince <princemeghani100@gmail.com>
2025-02-10 22:31:25 +0530

    statiuc routing first

--- routes/student.js
+++ routes/student.js
@@ -17,15 +17,23 @@ const {
     getAllEnrolledStudents,
     getNonCompulsorySubjects,
     assignAdditionalSubject,
-    
 } = require("../controllers/studentController");
-
 
 const { getClasses } = require("../controllers/classController");
 
-// Route to create a student (requires authentication)
+// Static Routes (Put these first)
+router.get("/class", authenticate , getClasses);
+router.put("/class/update", authenticate , updateClass);
+
+router.get("/section", authenticate , getSection);
+router.put("/section/update", authenticate , updateSection);
+
+router.get("/non-compulsory-subjects", authenticate, getNonCompulsorySubjects);
+
+// Dynamic Routes (Place these after static ones)
 router.post("/create", authenticate, createStudent);
-router.get("/enrollment-details",authenticate,getAllEnrolledStudents);
+router.get("/enrollment-details", authenticate, getAllEnrolledStudents);
+
 // Route to update the status of a student (requires authentication)
 router.patch("/:id/status", authenticate, updateStudentStatus);
 
@@ -45,14 +53,4 @@ router.get("/", authenticate, getAllStudents);
 router.get("/roll-number", authenticate , getRollNumber);
 router.put("/roll-number/update", authenticate , updateRollNumber);
 
-router.get("/class",authenticate , getClasses);
-router.put("/class/update", authenticate , updateClass);
-
-router.get("/section", authenticate , getSection);
-router.put("/section/update", authenticate , updateSection);
-
-
-router.get("/non-compulsory-subjects", authenticate, getNonCompulsorySubjects);
-
 module.exports = router;
-
